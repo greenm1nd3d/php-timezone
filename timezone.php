e1093b9e27835d698094b9eecc60b1249d8624c1
Fred R <j2reyes@yahoo.com>
2021-01-26 11:01:13 +0800

    Add PHP code

--- timezone.php
+++ timezone.php
@@ -4,12 +4,6 @@
     $curl = curl_init($url);
 
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, [
-      'X-RapidAPI-Host: kvstore.p.rapidapi.com',
-      'X-RapidAPI-Key: 7xxxxxxxxxxxxxxxxxxxxxxx',
-      'Content-Type: application/json'
-    ]);
-
     $response = curl_exec($curl);
     curl_close($curl);
 
